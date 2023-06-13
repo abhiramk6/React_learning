a8713bc5e915d7b9b93e3d9065f5b52cd7ec942b
abhiramk6 <2010080001@klh.edu.in>
2023-06-13 11:21:42 +0530

    added lower function

--- src/components/textForm.js
+++ src/components/textForm.js
@@ -8,7 +8,14 @@ export default function TextForm(props) {
 
     }
 
-    const handelOnClick =()=>{
+    const handelDoClick =()=>{
+
+      let newText =text.toLowerCase();
+      setText(newText);
+
+  }
+
+    const handelUpClick =()=>{
 
         let newText =text.toUpperCase();
         setText(newText);
@@ -22,7 +29,15 @@ export default function TextForm(props) {
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
-        <button className="btn btn-primary" onClick={handelOnClick}>convert to upercase</button>
+        <button className="btn btn-primary my-4 mx-4" onClick={handelUpClick}>convert to upercase</button>
+        <button className="btn btn-primary" onClick={handelDoClick}>convert to lowercase</button>
+        </div>
+
+        <div className="container">
+          <h3>Your text summary</h3>
+          <p>{text.split(" ").length} charecters and {text.length} words</p>
+          <h3>Preview</h3>
+          <p>{text}</p>
         </div>
     </div>
   )
